9a288e9e9cce7da3a859bee8f799b8c9e7a321c2
juanpolopurba <102169310+juanpolopurba@users.noreply.github.com>
2025-11-19 08:16:17 +0700

    Delete komentar.js

--- komentar.js
+++
@@ -1,9 +0,0 @@
-// Komentar satu baris
-
-/*
-    Komentar beberapa baris 
-    Menjelaskan bagian program
-*/
-
-let nama = "Lavender"; // Menyimpan nama siswa
-console.log("Nama saya", nama); //Tampilkan ke layar
