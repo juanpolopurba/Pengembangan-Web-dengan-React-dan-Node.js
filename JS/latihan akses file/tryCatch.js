8829bb9239100a4bf7aa92e149b9e35053c8445f
juanpolopurba <102169310+juanpolopurba@users.noreply.github.com>
2025-11-19 09:52:47 +0700

    Add files via upload

---
+++ JS/latihan akses file/tryCatch.js
@@ -0,0 +1,11 @@
+function bagiAngka(a, b) {
+    let hasil = a / b;
+    console.log("Hasil pembagian :", hasil)
+}
+
+let angka1 = 10;
+let angka2 = 0;
+
+bagiAngka(angka1, angka2);
+
+console.log("Program selesai");
