a3c528a9469d29b6cd11b8fba18c2ac661344a78
juanpolopurba <102169310+juanpolopurba@users.noreply.github.com>
2025-11-19 08:10:28 +0700

    Delete arrayBuah.js

--- arrayBuah.js
+++
@@ -1,6 +0,0 @@
-let buah = [];
-buah.push("Apel","Mangga", "Jeruk","Semangka","Pisang","Anggur","Nanas")
-buah.sort();
-for (let i = 0; i < buah.length; i++){
-    console.log(`${i + 1}. ${buah[i]}`);
-}
